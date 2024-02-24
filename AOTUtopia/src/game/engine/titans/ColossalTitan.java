1998a760321560daba6279b815f616cc5e407d34
rogereliass <93943757+rogereliass@users.noreply.github.com>
2024-02-24 22:24:07 +0200

    Update ColossalTitan.java

--- AOTUtopia/src/game/engine/titans/ColossalTitan.java
+++ AOTUtopia/src/game/engine/titans/ColossalTitan.java
@@ -7,7 +7,6 @@ public class ColossalTitan extends Titan {
 	public ColossalTitan(int baseHealth, int baseDamage, int heightInMeters, int distanceFromBase, int speed,
 			int resourcesValue, int dangerLevel) {
 		super(baseHealth, baseDamage, heightInMeters, distanceFromBase, speed, resourcesValue, dangerLevel);
-		// TODO Auto-generated constructor stub
 	}
 
 	public int getTITAN_CODE() {
