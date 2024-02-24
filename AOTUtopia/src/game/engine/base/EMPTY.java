f9e08f331622335937ff5c15565e37f3427cc9d8
monicasamaan <monicasamaan@172.20.10.13>
2024-02-24 20:17:39 +0200

    !!!!

--- AOTUtopia/src/game/engine/base/EMPTY.java
+++ AOTUtopia/src/game/engine/base/EMPTY.java
@@ -6,5 +6,6 @@ public class EMPTY {
 		//THIS IS AN EMPTY CLASS PLEASE IGNORE IT UNTIL NEEDED IN THE GAME
 		//The only purpose of this class is to make the package visible on github
 		//Kindly delete this class whenever any other class is implemented in this package
+		//testttttt
 	}
 }
