8a0f5db653318f1041cb4b656d66dd33775a59b8
Roger <Roger@ROGER.home>
2024-02-24 21:40:19 +0200

    Done 4.1 - 4.5

---
+++ AOTUtopia/src/game/engine/titans/ArmoredTitan.java
@@ -0,0 +1,16 @@
+package game.engine.titans;
+
+public class ArmoredTitan extends Titan{
+
+	final private int TITAN_CODE = 3;
+	
+	public ArmoredTitan(int baseHealth, int baseDamage, int heightInMeters, int distanceFromBase, int speed,
+			int resourcesValue, int dangerLevel) {
+		super(baseHealth, baseDamage, heightInMeters, distanceFromBase, speed, resourcesValue, dangerLevel);
+	}
+
+	public int getTITAN_CODE() {
+		return TITAN_CODE;
+	}
+
+}
---
+++ AOTUtopia/src/game/engine/titans/ColossalTitan.java
@@ -0,0 +1,17 @@
+package game.engine.titans;
+
+public class ColossalTitan extends Titan {
+	
+	final private int TITAN_CODE = 4;
+
+	public ColossalTitan(int baseHealth, int baseDamage, int heightInMeters, int distanceFromBase, int speed,
+			int resourcesValue, int dangerLevel) {
+		super(baseHealth, baseDamage, heightInMeters, distanceFromBase, speed, resourcesValue, dangerLevel);
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getTITAN_CODE() {
+		return TITAN_CODE;
+	}
+
+}
