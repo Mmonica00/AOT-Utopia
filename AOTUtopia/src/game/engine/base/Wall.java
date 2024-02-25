c35571285b50a2caf9bbfa10ff1064f1731900f4
monicasamaan <monicasamaan@172.20.10.13>
2024-02-25 20:20:38 +0200

    4.11

---
+++ AOTUtopia/src/game/engine/base/Wall.java
@@ -0,0 +1,29 @@
+package game.engine.base;
+
+public class Wall {
+	private int baseHealth;
+	private int currentHealth;
+	
+	public Wall(int baseHealth) {
+		super();
+		this.baseHealth = baseHealth;
+		this.currentHealth = baseHealth;
+	}
+
+	public int getCurrentHealth() {
+		return currentHealth;
+	}
+
+	public void setCurrentHealth(int currentHealth) {
+		this.currentHealth = currentHealth;
+	}
+
+	public int getBaseHealth() {
+		return baseHealth;
+	}
+	
+	
+	
+	
+	
+}
---
+++ AOTUtopia/src/game/engine/lanes/Lane.java
@@ -0,0 +1,5 @@
+package game.engine.lanes;
+
+public class Lane {
+	
+}
