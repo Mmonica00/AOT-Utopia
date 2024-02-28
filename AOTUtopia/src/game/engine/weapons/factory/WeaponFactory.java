3c816da374326e0b848afea58b6028a33fd5a0cb
monicasamaan <monicasamaan@172.20.10.13>
2024-02-28 20:37:39 +0200

    final edit

--- AOTUtopia/src/game/engine/weapons/factory/WeaponFactory.java
+++ AOTUtopia/src/game/engine/weapons/factory/WeaponFactory.java
@@ -2,11 +2,12 @@ package game.engine.weapons.factory;
 import java.io.IOException;
 import java.util.HashMap;
 import game.engine.weapons.WeaponRegistry;
-
+import game.engine.dataloader.DataLoader;
 public class WeaponFactory {
-	private HashMap<Integer, WeaponRegistry> weaponShop; //FIXME:This attribute is initialized once the data is read and is READ ONLY 
+	private final HashMap<Integer, WeaponRegistry> weaponShop; //FIXME:This attribute is initialized once the data is read and is READ ONLY 
 	
 	WeaponFactory() throws IOException{
-		
+		weaponShop = DataLoader.readWeaponRegistry();
 	}
+	
 }
