c4c72491b58a791a3b7a359c7c942fa133285f26
monicasamaan <monicasamaan@192.168.1.101>
2024-02-29 17:32:15 +0200

    5.4 done

---
+++ AOTUtopia/src/game/engine/exceptions/InvalidCSVFormat.java
@@ -0,0 +1,20 @@
+package game.engine.exceptions;
+
+import java.io.IOException;
+
+@SuppressWarnings("serial")
+public class InvalidCSVFormat extends IOException{
+	private static final String MSG = "Invalid input detected while reading csv file, input = \n";
+	private String inputLine;
+	
+	public InvalidCSVFormat(String inputLine) {
+		super(MSG + inputLine);
+		this.inputLine = inputLine;
+	}
+	public InvalidCSVFormat(String message, String inputLine) {
+		super(message);
+		this.inputLine = inputLine;
+	}
+	
+
+}
