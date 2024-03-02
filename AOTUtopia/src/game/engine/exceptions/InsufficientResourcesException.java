725e96b042704c9e457e0277b0ba5661311d211d
Roger <Roger@ROGER.home>
2024-03-02 19:44:54 +0200

    edit

---
+++ AOTUtopia/src/game/engine/exceptions/InsufficientResourcesException.java
@@ -0,0 +1,28 @@
+package game.engine.exceptions;
+
+@SuppressWarnings("serial")
+public class InsufficientResourcesException extends GameActionException{
+
+	private static final String MSG = "Not enough resources, resources provided =";
+	private int resourcesProvided;
+	
+	public InsufficientResourcesException(int resourcesProvided) {
+		super(MSG + resourcesProvided);
+		this.setResourcesProvided(resourcesProvided);
+	}
+	
+	public InsufficientResourcesException(String message,int resourcesProvided) {
+		super(message + resourcesProvided);
+		this.setResourcesProvided(resourcesProvided);
+	}
+
+	public int getResourcesProvided() {
+		return resourcesProvided;
+	}
+
+	public void setResourcesProvided(int resourcesProvided) {
+		this.resourcesProvided = resourcesProvided;
+	}
+	
+	
+}
