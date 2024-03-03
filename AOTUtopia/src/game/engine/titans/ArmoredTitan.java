863bff35c3b10bead9067d3723ef1a507c78901d
Admin <Admin@192.168.1.8>
2024-03-03 20:36:23 +0200

    yaay

--- AOTUtopia/src/game/engine/titans/ArmoredTitan.java
+++ AOTUtopia/src/game/engine/titans/ArmoredTitan.java
@@ -2,7 +2,7 @@ package game.engine.titans;
 
 public class ArmoredTitan extends Titan{
 
-	private final static int TITAN_CODE = 3;
+	public final static int TITAN_CODE = 3;
 	
 	public ArmoredTitan(int baseHealth, int baseDamage, int heightInMeters, int distanceFromBase, int speed,
 			int resourcesValue, int dangerLevel) {
--- AOTUtopia/src/game/engine/titans/ColossalTitan.java
+++ AOTUtopia/src/game/engine/titans/ColossalTitan.java
@@ -2,7 +2,7 @@ package game.engine.titans;
 
 public class ColossalTitan extends Titan {
 	
-	private final static int TITAN_CODE = 4;
+	public final static int TITAN_CODE = 4;
 
 	public ColossalTitan(int baseHealth, int baseDamage, int heightInMeters, int distanceFromBase, int speed,
 			int resourcesValue, int dangerLevel) {
