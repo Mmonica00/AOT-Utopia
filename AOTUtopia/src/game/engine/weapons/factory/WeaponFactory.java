aef9f347ac2887bf1e00e45802c7a357f6722f9d
Roger <Roger@ROGER.home>
2024-03-04 11:26:46 +0200

    edit

--- AOTUtopia/src/game/engine/weapons/factory/WeaponFactory.java
+++ AOTUtopia/src/game/engine/weapons/factory/WeaponFactory.java
@@ -1,8 +1,10 @@
 package game.engine.weapons.factory;
+
 import java.io.IOException;
 import java.util.HashMap;
 import game.engine.weapons.WeaponRegistry;
 import game.engine.dataloader.DataLoader;
+
 public class WeaponFactory {
 	
 	private final HashMap<Integer, WeaponRegistry> weaponShop; 
